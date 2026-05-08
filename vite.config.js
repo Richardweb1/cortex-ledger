540566e0d63fdbf9949c0a061dca0af5c3ffaafa
Richardweb1 <adrdour1rachid@gmail.com>
2026-05-08 18:35:10 +0100

    fix multi page build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: 'public',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: 'public/index.html',
+        submit: 'public/submit.html'
+      }
+    }
   },
   server: {
     port: 5173,
